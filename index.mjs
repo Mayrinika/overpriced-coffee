aca37f909c8a9016a4fa21e8cfa48a7aca8bc8fb
Mayrinika <mashuliy_u@mail.ru>
2020-12-07 22:52:26 +0500

    task 6

--- index.mjs
+++ index.mjs
@@ -8,66 +8,63 @@ const port = 3000;
 const app = express();
 
 const menu = [
-  { name: "Americano",        image: "/static/img/americano.jpg",       price: 999,},
-  { name: "Cappuccino",       image: "/static/img/cappuccino.jpg",      price: 999 },
-  { name: "Espresso",         image: "/static/img/espresso.jpg",        price: 999 },
-  { name: "Flat-white",       image: "/static/img/flat-white.jpg",      price: 999 },
-  { name: "Latte-macchiato",  image: "/static/img/latte-macchiato.jpg", price: 999 },
-  { name: "Latte",            image: "/static/img/latte.jpg",           price: 999 },
+    {name: "Americano", image: "/static/img/americano.jpg", price: 999,},
+    {name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999},
+    {name: "Espresso", image: "/static/img/espresso.jpg", price: 999},
+    {name: "Flat-white", image: "/static/img/flat-white.jpg", price: 999},
+    {name: "Latte-macchiato", image: "/static/img/latte-macchiato.jpg", price: 999},
+    {name: "Latte", image: "/static/img/latte.jpg", price: 999},
 ];
 
-const cart = [];
+let cart = [];
 
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
 // Настраиваем пути и дефолтный view
 app.engine(
-  "hbs",
-  hbs({
-    extname: "hbs",
-    defaultView: "default",
-    layoutsDir: path.join(rootDir, "/views/layouts/"),
-    partialsDir: path.join(rootDir, "/views/partials/"),
-  })
+    "hbs",
+    hbs({
+        extname: "hbs",
+        defaultView: "default",
+        layoutsDir: path.join(rootDir, "/views/layouts/"),
+        partialsDir: path.join(rootDir, "/views/partials/"),
+    })
 );
 
 app.use('/static', express.static('static'));
 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
-  res.redirect('/menu');
+    res.sendFile(path.join(rootDir, "/static/html/index.html"));
+    res.redirect('/menu');
 });
 
 app.get("/menu", (_, res) => {
-  res.render("menu", {
-    layout: "default",
-    items: menu,
-  });
+    res.render("menu", {
+        layout: "default",
+        items: menu,
+    });
 });
 
 app.get("/cart", (_, res) => {
-  res.render("cart", {
-    layout: "default",
-    items: cart,
-  });
+    res.render("cart", {
+        layout: "default",
+        items: cart,
+    });
 });
 
 app.get("/buy/:name", (req, res) => {
-  const coffe = menu.find(menuItem => menuItem.name === req.params.name);
-  cart.push(coffe);
-  res.redirect('/menu');
-});
-
-app.get("/cart", (req, res) => {
-  res.status(501).end();
+    const coffe = menu.find(menuItem => menuItem.name === req.params.name);
+    cart.push(coffe);
+    res.redirect('/menu');
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+    cart = [];
+    res.redirect('/cart');
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+    res.status(501).end();
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
